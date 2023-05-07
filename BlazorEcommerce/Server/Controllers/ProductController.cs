0d38d212a9e89453c488e1258d6f3a254a2b4c9b
Chase Ingersol <chase@ingersol.io>
2023-05-07 15:58:56 -0700

    Add ProductService to handle fetching product list

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -5,19 +5,17 @@ namespace BlazorEcommerce.Server.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase {
-        private readonly DataContext _context;
 
-        public ProductController(DataContext context) {
-            _context = context;
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService) {
+            _productService = productService;
         }
        
         [HttpGet]
-        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct() {
-            var products = await _context.Products.ToListAsync();
-            var response = new ServiceResponse<List<Product>>() {
-                Data = products
-            };
-            return Ok(response);
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts() {
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
         }
     }
 }
